536b1fbcb9722b4bce339007beaa6e611a82a67e
ĽudmilaMlynarčíková <ludmila.mlynarcikova@herki.com>
2024-08-24 22:00:52 +0200

    Values, Variables and Data Types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,70 @@
+/*
+//////////////////////////////////////////////////////
+// Values and Variables
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("John");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let PI = 3.14115;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+
+//////////////////////////////////////////////////////
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "John");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); //it should return null, but if is returning object, which is an error in JS
+
+*/
+//////////////////////////////////////////////////////
+// Let, const and var
+
+
+// Assignemt
+let myCountry = "Slovakia";
+console.log(myCountry);
+
+let myContinent = "Europe";
+console.log(myContinent);
+
+let population = 5.6;
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof myCountry);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
