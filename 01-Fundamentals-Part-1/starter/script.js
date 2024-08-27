b37c970918a376ccecd580004ede726c89390f12
ĽudmilaMlynarčíková <ludmila.mlynarcikova@herki.com>
2024-08-27 15:13:08 +0200

    If/else statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -45,26 +45,162 @@ console.log(typeof year);
 
 console.log(typeof null); //it should return null, but if is returning object, which is an error in JS
 
-*/
+
 //////////////////////////////////////////////////////
-// Let, const and var
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+
+var job = "programmer";
+job = "teacher";
 
 
-// Assignemt
-let myCountry = "Slovakia";
-console.log(myCountry);
+//////////////////////////////////////////////////////
+// Basic Operators
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSara = now - 2020;
+console.log(ageJonas, ageSara);
 
-let myContinent = "Europe";
-console.log(myContinent);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 2 ** 3 -> 2 to the power of 3 -> 2 * 2 * 2
 
-let population = 5.6;
-console.log(population);
+const firstName = "Jonas";
+const lastName = "Smith";
+console.log(firstName + " " + lastName);
 
-let isIsland = false;
-let language;
+// Assignment Operators
+let x = 10 + 5; // we have 2 types of operators = & +; result = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + x = 101
+x--; // x = x - x = 100
+x /= 2; // x = x / 2 = 50
+console.log(x);
 
-console.log(typeof myCountry);
-console.log(typeof population);
-console.log(typeof isIsland);
-console.log(typeof language);
+// Comparison Operators
+// >, <, >=, <=
+console.log(ageJonas > ageSara); // true
+console.log(ageSara >= 18); // false
 
+const isFullAge = ageSara >= 18;
+
+//Operator Precedence
+let z, y;
+z = y = 25 - 10 - 5; // z = y = 10, z = 10
+console.log(z, y);
+
+const averageAge = (ageJonas + ageSara) / 2; // prednosť majú operácie ako v matematike, zátvorky idú prvé a potom delenie
+console.log(averageAge);
+
+
+//////////////////////////////////////////////////////
+// Strings and Template Literals
+const firstName = "Lu";
+const job = "programmer";
+const birthYear = 1997;
+const year = 2024;
+
+const introduction = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(introduction);
+
+const introductionNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(introductionNew);
+
+console.log(`This is just a regular string...`);
+
+console.log(`String with
+multiple
+lines!`);
+
+
+//////////////////////////////////////////////////////
+// If/else statement
+const age = 15;
+
+if (age >= 18) {
+    console.log("Sarah can start driving license!");
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years.`);
+}
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000 && birthYear > 1900) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+
+//////////////////////////////////////////////////////
+// Type Conversion
+const inputYear = "1991"; // 1991 as string
+console.log(Number(inputYear)); // 1991 as number
+
+const numberInputYear = Number(inputYear);
+console.log(numberInputYear + 18); // 2009
+console.log("Jonas"); // NaN - Not a Number is still a number, but invalid one
+console.log(String(52)); // 52 as string -> converting number to string
+
+// Type Coercion
+console.log("I'm" + 23 + " years old."); // automaticky prehodí number na string vďaka +
+console.log('23' - '10' - 3); // opposit coercion, convert string to numbers -> result 10
+console.log('23' / '2'); // 11.5
+
+let n = '1' + 1; // '11' as string
+n = n - 1; // 11 convert to number and then decrease the number by 1
+console.log(n); // 10 as number
+
+let p = 2 + 3 + 4 + '5'; // 2 + 3 + 4 = 9 as number, '5' as string
+console.log(p); // '95' as string
+*/
+
+//////////////////////////////////////////////////////
+// Truthy and Falsy Values
+
+
+//////////////////////////////////////////////////////
+// Assignment
+console.log("-----Assignment-----");
+const myCountry = "Slovakia";
+const myContinent = "Europe";
+let population = 125.43;
+const isIsland = false;
+const language = "slovak";
+
+const halfPopulation = population / 2;
+console.log(halfPopulation);
+
+const increasePopulation = population + 1;
+console.log(increasePopulation);
+
+const finlandPopulation = 6;
+const averagePopulation = 33;
+console.log(population > finlandPopulation);
+console.log(averagePopulation > population);
+
+const description = myCountry + " is in " + myContinent + ", and its " + population + " million people speak " + language + ".";
+console.log(description);
+
+const descriptionNew = `${myCountry} is in ${myContinent}, and its ${population} million people speak ${language}.`;
+console.log(descriptionNew);
+
+let calculation;
+if (population > averagePopulation) {
+    calculation = population - averagePopulation;
+    console.log(`Populaion of ${myCountry} is ${calculation} million above average!`)
+} else {
+    calculation = averagePopulation - population;
+    console.log(`Population of ${myCountry} is ${calculation} below average!`)
+}
+
+console.log('9' - '5'); // -> '4' as string
+console.log('19' - '13' + '17'); // -> '617' as string
+console.log('19' - '13' + 17); // -> 23 as number
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143 as number
